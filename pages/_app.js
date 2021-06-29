559364d67ebbcc62b72c6521aecd0c8325cf7464
Salih <sudanmerinosu@gmail.com>
2021-06-29 16:37:18 +0300

    Overall layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,25 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<div>
-			<nav>
-				<ul>
-					<li>
-						<Link href='/'>
-							<a> AnaSayfa</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/about'>
-							<a>Hakkımda</a>
-						</Link>
-					</li>
-				</ul>
-			</nav>
+		<div className='mx-auto w-9/12 my-8'>
+			<header>
+				<h1 className='text-6xl font-bold text-center'>Özgür Yazar</h1>
+				<nav className='my-4'>
+					<ul className='flex flex-row space-x-4 justify-center'>
+						<li>
+							<Link href='/'>
+								<a> AnaSayfa</a>
+							</Link>
+						</li>
+						<li>
+							<Link href='/about'>
+								<a>Hakkımda</a>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
 			<Component {...pageProps} />
 		</div>
 	);
